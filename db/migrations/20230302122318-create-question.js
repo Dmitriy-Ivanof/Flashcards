a68209f751247e2f4bb55fc49c087c4b2e729117
Dmitriy-Ivanof <dmitrii_i@bk.ru>
2023-03-02 16:33:49 +0300

    second

---
+++ db/migrations/20230302122318-create-question.js
@@ -0,0 +1,41 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Questions", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      question: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      answer: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      topicId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Topics",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Questions");
+  },
+};
---
+++ db/seeders/20230302124223-topic.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Topics",
+      [
+        {
+          name: "Группа Бобры ElbrusBootCamp - SPb",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Умные и красивые",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Topics", null, {});
+  },
+};
